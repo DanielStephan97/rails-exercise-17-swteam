d27dd4a8f561b387c0f847ba058a3f1a82307e29
DanielStephan97 <d.stephan97@gmx.de>
2017-10-29 12:05:35 +0000

    Added failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,13 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should have an input field 'First name', 'Last name' and 'Homepage'" do
+    visit new_author_path 
+
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
+  end
+
 end
