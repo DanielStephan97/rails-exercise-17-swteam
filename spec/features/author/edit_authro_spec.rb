d76558229d5dad1c2f520536ffbadc30a26d6cb8
DanielStephan97 <dstephan97@gmx.de>
2017-10-30 13:17:54 +0000

    Changed test for #14. The test now first creates an author and afterwards tries to edit it.

--- spec/features/author/edit_authro_spec.rb
+++ spec/features/author/edit_authro_spec.rb
@@ -1,7 +1,15 @@
-describe 'Edit Author page', type: :feature do
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
 
 	it "should render" do
-		visit edit_author_path
+		visit new_author_path
+		
+		fill_in "author_first_name", :with => "Alan"
+		fill_in "author_last_name", :with => "Turing"
+		fill_in "author_homepage", :with => "http://wikipedia.org/Alan_Turing"
+		find('input[type="submit"]').click
+		page.find_link('Edit').click
 	end
 
 end
