2aacbee754c9d75fd68baeb725ffb1f153df541f
DanielStephan97 <dstephan97@gmx.de>
2017-10-30 10:48:57 +0000

    Added test for #11, as I did not read #10 corretly. Also added a 'Show' link ('index'->author_details) for every author. Closes #11.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -7,4 +7,24 @@ describe "Author index page", type: :feature do
 		expect(page).to have_link('Add author')
 	end
 	
+	it "should have a link for each author with text 'Show'" do
+		create_author()
+		create_author()
+		
+		visit authors_path
+		page.all('tr').each do |tr|
+			next if tr.has_text?('Name')
+			expect(tr).to have_link('Show')
+		end
+	end
+	
+	private
+	def create_author() 
+		visit new_author_path
+		fill_in "author_first_name", :with => "Alan"
+		fill_in "author_last_name", :with => "Turing"
+		fill_in "author_homepage", :with => "http://wikipedia.org/Alan_Turing"
+		find('input[type="submit"]').click
+	end
+	
 end
