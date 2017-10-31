61e726ce439a1b2be4a86041c7328e9c3e429295
DanielStephan97 <dstephan97@gmx.de>
2017-10-31 08:16:04 +0000

    Added failing test for #18

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 describe "New paper page", type: :feature do
   
-  it "should render withour error" do
-    visit new_paper_path
-  end
-  
-  it "should have a field 'Title'" do
-	visit new_paper_path
-	expect(page).to have_field('Title')
-  end
+	it "should render withour error" do
+		visit new_paper_path
+	end
+
+	it "should have a field 'Title'" do
+		visit new_paper_path
+		expect(page).to have_field('Title')
+	end
+
+	it "shuld hava a field 'Venue'" do
+		visit new_paper_path
+		expect(page).to have_field('Venue')
+	end
+
+	it "should have a field 'Year'" do
+		visit new_paper_path
+		expect(page).to have_field('Year')
+	end
 
 end
