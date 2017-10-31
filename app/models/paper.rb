a50aed94acbd407eb1bfcb91dbd770df112c1c32
DanielStephan97 <dstephan97@gmx.de>
2017-10-31 10:05:11 +0000

    Added tests for #20, #21 just in case even if this one line is trivial. Also added validation. Closes #21.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,5 +3,7 @@ class Paper < ActiveRecord::Base
 	validates :title, presence: true
 	
 	validates :venue, presence: true
+	
+	validates :year, presence: true
 
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,7 +3,26 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
   
   it "should not validate without a Title" do
-	expect(Paper.create()).not_to be_valid
+	@paper = paper
+	@paper.title = nil
+	expect(@paper).not_to be_valid
+  end
+  
+  it "should not validate without a Venue" do
+	@paper = paper
+	@paper.venue = nil
+	expect(@paper).not_to be_valid
+  end
+  
+  it "should not validate without a Year" do
+	@paper = paper
+	@paper.year = nil
+	expect(@paper).not_to be_valid
+  end
+  
+  private 
+  def paper
+	FactoryGirl.create :paper
   end
   
 end
