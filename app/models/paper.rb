7258faf90f70fb9aefb92f07661db2dd3b79ed58
DanielStephan97 <dstephan97@gmx.de>
2017-10-31 09:27:50 +0000

    Added failing test for #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,5 @@
 class Paper < ActiveRecord::Base
+
+	
+
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should not validate without a Title" do
+	expect(Paper.create()).not_to be_valid
+  end
+  
 end
