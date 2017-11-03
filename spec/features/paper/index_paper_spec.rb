8972ec3a6f5b81edc5fce5645a0d0749cfd40d09
DanielStephan97 <d.stephan97@gmx.de>
2017-11-03 21:25:30 +0000

    Added failing test for #28.

---
+++ spec/features/paper/index_paper_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Index Paper page", type: :feature do
+
+	it "should only show papers that satisfy the url filter" do
+		
+		@paper1 = FactoryGirl.create :paper
+		@paper1.year = 1950
+		@paper2 = FactoryGirl.create :paper
+		@paper2.year = 1968
+		visit papers_path
+		visit '/?year=1950'
+		expect(page).not_to have_content("1968")
+		expect(page).to have_content("1950")
+	end
+
+end
